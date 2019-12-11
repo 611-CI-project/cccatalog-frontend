34bcbbe26147535c4406eaebbc368d1ada633ea2
Maddis87 <madeleneamberman@gmail.com>
2019-12-11 19:04:40 +0100

    exec .sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
-#!/bin/sh -l
+  
+#!/bin/sh
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_CONF" \
+--output $HOME/secrets/admin.conf ./secrets/admin.conf.gpg
