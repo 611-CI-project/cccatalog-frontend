d2e1d5150a035da901e1e424a4b8591e347e9550
Maddis <madeleneamberman@gmail.com>
2019-12-26 12:15:22 +0100

    trying to display new changes

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -3,8 +3,8 @@
   <header-section showNavSearch="true"></header-section>
   <div class="page_body">
     <h1>About CC Search</h1>
-      <p class="page_lead-paragraph">
-        hejsan CC Search is a tool that allows openly licensed and public domain works to be discovered
+      <p class="page_lead">
+        CC Search is a tool that allows openly licensed and public domain works to be discovered
         and used by everyone. Creative Commons, the nonprofit behind CC Search, is the maker of the
         <a href="https://creativecommons.org/share-your-work/licensing-types-examples/">CC licenses</a>,
         used over 1.4 billion times to help creators share knowledge and creativity online.
